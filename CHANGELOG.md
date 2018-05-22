0b479eeae5713af4c738d365d0e128f0e456dc31
Evan Haxton <ehaxton@gmail.com>
2018-05-22 11:29:37 -0400

    Inital development

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# 1_MongoDB Cookbook ChangeLog
+
+# 0.1.0
+
+- Initial development
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: 1_MongoDB
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
