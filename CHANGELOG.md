797fae312bc834b3810ac51c10d6452b5d3d91e3
Evan Haxton <ehaxton@gmail.com>
2018-05-22 12:31:30 -0400

    Removed integrated build with Travis CI

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,12 @@
 # 1_MongoDB Cookbook ChangeLog
 
+# 0.1.2
+
+- Removed integration build with Travis CI.  Initial subscription exceeded
+
 # 0.1.1
 
-- - Integrated build with Travis CI
+- Integrated build with Travis CI
 
 # 0.1.0
 
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Evan Haxton'
 maintainer_email 'ehaxton@gmail.com'
 license 'apachev2'
 description 'Installs/Configures a simple MongoDB installation'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
